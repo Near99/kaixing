c1fbf082bf63ca2aad5d800aabf179f2e5c31e64
Near99 <wkx0823@gmail.com>
2021-08-30 22:30:31 +0800

    test

---
+++ src/data.js
@@ -0,0 +1,314 @@
+export const data = {
+  english: {
+    nav: {
+      about: "About",
+      project: "Project",
+      resume: "Resume",
+      contact: "Contact",
+    },
+    home: {
+      nameHeading: `I'm Kaixing Wang`,
+      briefIntroOne: `
+      Hello there, I am a self-taught full-stack developer who has been learning by coding full-time via varieties of both paid and free resources online at home for three months. I have developed fairly good foundations with frontend technologies, and I am able to build functional web applications. Currently I am learning backend technologies starting with Node.js and MongoDB after finishing working with JavaScript (ES6+), React, HTML5, CSS3 in the browser.
+        `,
+      briefIntroTwo: `There is also a lot of time I spent on computer fundamental knowledge such as Data Structure and Algorithms, Computer networks, Operating systems, and Databases. I aim to be a real software engineer rather than a developer who can only code but knowing nothing of what is happening under the hood. You can go to the About page to check what I have been learning so far in more detail by clicking the link below. Thank you.`,
+      moreAboutMe: "Learn More About Me",
+    },
+    about: {
+      aboutTitle: "About Me.",
+      aboutInfo: [
+        {
+          left: "Status Update, 30th August 2021",
+          right: "Be better",
+          link: "https://google.com",
+          p: `I am the kind of person who has lots of curiosities about the core, fundamental concepts of the things I want to learn and study. I believe that no comprehensive understanding of what is under the hood results in no good outcomes when trying to master a skill, and no exceptions can go through this natural principle. I have always been amazed and shocked how some tools, libraries, frameworks are built while coding in a higher level abstraction with them. Since I have been learning for roughly four months, I gradually lost interest in building something that people who know nothing about programming may be surprised of but really just few APIs are used. I am fully aware being a real software engineer isn’t about how many programming languages you know, what kind of applications you can build. Coding in higher level abstractions can’t bring much to oneself. With that being said, I decided to switch my concentration from ‘superstructure’ to ‘substrate’ such as Computer Architecture, Structure and Interpretation of Computer Programs, Algorithms and Data Structures. I don’t wanna be the person who never quite understood something like recursion, really. Of course I will still spend some time on frontend technologies but will be trying to build tools, simple wheels, not applications.`,
+        },
+        {
+          left: "Education Background",
+          right: "Royal Hollway, University of London",
+          link: "https://www.royalholloway.ac.uk/",
+          p: `I completed my undergraduate studies in Management with Marketing from Royal Hollway, University of London with a 2:1 honor in October 2020, and had my hands on it in July 2021. Yes it took me nearly one year to get it thanks to Covid.`,
+        },
+        {
+          left: "Why programming?",
+          right: "Programming is cool",
+          link: "https://teachmag.com/archives/7626",
+          p: `I have always been interested in programming and computer science since I was little. For some reasons, however, I wasn’t able to do a computer science related degree at uni. 
+Luckly, Covid ruined everything I had planned after graduation (lol), and we were informed the degree cert would not be sent to us any time soon. Things started to get interesting, I told myself. I said alright, without any further ado, I am gonna learn how to code. But how? Well, I picked up the cheapest and most challenging way: teaching myself. And, there was a further ado, I took a long break first (lol). It is really tough and difficult. There were many times I wanted to give it up. Thanks to myself, I have come this far, and I don’t mind going a little bit further. I will list all the skills I know and all resources I have used learning these skills from.
+`,
+        },
+        {
+          left: "CS50 - Introduction to Computer Science (free)",
+          right: "View CS50",
+          link: "https://online-learning.harvard.edu/course/cs50-introduction-computer-science?delta=0",
+          p: `This is the first course I took, by far the most popular and best (in my opinion) free intro course on the internet. I really enjoyed this course,  the curriculum is well structured, and David delivered it in a very fantastic way. I gained a broad and robust understanding of computer science and programming, how to think algorithmically and solve programming problems efficiently. The teaching language is primarily in C, and Python, PHP, JavaScript plus SQL, CSS, and HTML are introduced in the last few lectures. I spent around two weeks on this course and completed three problem sets. Skipped remaining psets for time saving.`,
+        },
+        {
+          left: "freeCodeCamp (free)",
+          right: "View freeCodeCamp",
+          link: "https://www.freecodecamp.org/",
+          p: `Learned basic of HTML, CSS and responsive websites. Studied a bit of JavaScript there as well but switched to the Odin Project for a better structured curriculum. Even I don’t quite like freeCodeCamp contents, I sometimes visit their Youtube channel for some practical tutorials, never finishing watching any entire video though because it’s too long.`,
+        },
+        {
+          left: "The Odin Project free full stack bootcamp (free)",
+          right: "View The Odin Project",
+          link: "https://www.theodinproject.com/dashboard",
+          p: `This is the main roadmap I have been following. It provides a better curriculum as mentioned above. The nice thing about Odin is their exercise projects which are really good and real world based. I, however, intentionally skipped some projects because either I didn’t like it (e.g. todo list) or I was able to build the functionalities with JavaScript but had zero interest in styling the page to make it look better and submit it (e.g. weather app etc.). As of 25th August, I have completed foundation, JavaScript sections and moved on to backend now(Node.js Path).`,
+        },
+        {
+          left: "Ben Awad - Youtube channel (free)",
+          right: "View Ben's channel",
+          link: "https://www.youtube.com/c/BenAwad97",
+          p: `Ben is a pro open source programmer and technical consultant. He provides full stack project tutorials building things with React, Node. His React tutorial is my favorite but some of his contents are a bit advanced for me.`,
+        },
+        {
+          left: "Clement Mihailescu - Youtube channel (free)",
+          right: "View Clement's channel",
+          link: "https://www.youtube.com/channel/UCaO6VoaYJv4kS-TQO_M-N_g",
+          p: `Clement got himself into Google after 6 months of immersive study in a bootcamp without writing a single line of code before. His story is inspiring but he has a degree of math from University of pennsylvania… I love this guy’s video for mindset.`,
+        },
+        {
+          left: "Algoexpert",
+          right: "View Algoexpert",
+          link: "https://www.algoexpert.io/product",
+          p: `Algoexpert was created by Clement Mihailescu above, which is a place for people who want to learn essential algorithms for coding interviews. Full video explanation for each question in detail. Super nice. `,
+        },
+        {
+          left: "MDN Web Docs (free)",
+          right: "View MDN",
+          link: "https://developer.mozilla.org/en-US/",
+          p: `MDN is the place I go most often whenever I need to refer to some Web APIs. I also took some tutorials there.`,
+        },
+        {
+          left: "JavaScript30 - Wes Box (free)",
+          right: "View JavaScript30",
+          link: "https://javascript30.com/",
+          p: `30 Vanilla JavaScript little projects practice in 30 days without any libraries and frameworks. It’s been I don’t know how many days since I found it on github but I haven’t completed this course till now as there are always other fun things to do. My bad and I am gonna try to finish it asap.`,
+        },
+        {
+          left: "Traversy Media - Youtube channel (free)",
+          right: "View Traversy's channel",
+          link: "https://www.youtube.com/c/TraversyMedia",
+          p: `Varieties of Web development technologies tutorials. Countless practical projects. I watched his 
+contents at the very beginning because it’s too practical, lack of good concept explanations. 
+`,
+        },
+        {
+          left: "Ben Eater - Youtube channel (free)",
+          right: "View Ben's channel",
+          link: "https://www.youtube.com/c/BenEater",
+          p: `Ben Eater’s networking tutorial is probably the best on Youtube. 13 episodes in total, gives me an overall view of how networking is. He covers lots of stuff about hardware as well.`,
+        },
+        {
+          left: "JavaScript: Understanding the Weird Parts - Anthony Alicea",
+          right: "View this course",
+          link: "https://www.udemy.com/course/understand-javascript/",
+          p: `Best JavaScript fundamental concepts course I have ever taken. Tony helps me to grasp how JavaScript works under the hood, things like Syntax Parsers, Execution Contexts and Lexical Environments. I gained a better understanding of advanced concepts such as Closures, Prototypal inheritance, IIFEs.`,
+        },
+        {
+          left: "Promises, Async, and Await - Tony Alicea Youtube channel (free)",
+          right: "View on Youtube",
+          link: "https://www.youtube.com/watch?v=fyGSyqEX2dw&t=1156s",
+          p: `Best JavaScript Promise explanation for me. I felt like starting to understand Promise, and being able to build a very simple Promise following  the tutorial.`,
+        },
+        {
+          left: "What the heck is the event loop anyway? - Philip Roberts | JSConf EU",
+          right: "View on Youtube",
+          link: "https://www.youtube.com/watch?v=8aGhZQkoFbQ&t=479s",
+          p: `Best event loop explanation for me. Very easy to understand and get the point.`,
+        },
+        {
+          left: "A Complete Guide to Flexbox - CSS Tricks",
+          right: "View this guide",
+          link: "https://css-tricks.com/snippets/css/a-guide-to-flexbox/",
+          p: `I never learned CSS seriously. This is one of the few tutorials about CSS I learned and It’s pretty good. I use flex box all the time since I read it, and always get back to this article cause I always forget things when dealing with CSS.`,
+        },
+      ],
+    },
+    project: {
+      projectTitle: "Little things I have done.",
+      projectInfo: [
+        {
+          left: "CV-Generator",
+          right: "View Project",
+          link: "https://near99.github.io/cv-project/",
+          p: `A CV generator project built with React. Real time update inputs to CV preview template.`,
+        },
+        {
+          left: "Personal Portfolio Website",
+          right: "View Project",
+          link: "https://near99.github.io/personal-portfolio/",
+          p: `A responsive personal portfolio website built with React. Able to switch languages, light and dark themes. `,
+        },
+        {
+          left: "Mahjong Fan Calculator",
+          right: "View Project",
+          link: "https://near99.github.io/mahjong_calculator/",
+          p: `A simple International Mahjong fan calculator written in vanilla JavaScript. Real time displaying current ranking, game details. Uses localstorage.`,
+        },
+        {
+          left: "Booklist",
+          right: "View Project",
+          link: "https://near99.github.io/library/",
+          p: `A simple booklist written in vanilla JavaScript. Form validation and CSS grid practice.`,
+        },
+        {
+          left: "Calculator",
+          right: "View Project",
+          link: "https://near99.github.io/calculator/",
+          p: `A very basic calculator written in vanilla JavaScript. JS Class and CSS Flexbox practice.`,
+        },
+        {
+          left: "Etch A Sketch",
+          right: "View Project",
+          link: "https://near99.github.io/etch-a-sketch/",
+          p: `Simple Etch A Sketch written in vanilla JavaScript. DOM manipulation practice.`,
+        },
+      ],
+    },
+    contact: {
+      title: "Get in touch",
+      contactInfo: [
+        { left: "Phone", right: "+86-0120-2433", address: "+86-0120-2433" },
+        { left: "Wechat", right: "18601202433", address: "18601202433" },
+        {
+          left: "Email",
+          right: "wkx0823@gmail.com",
+          address: "wkx0823@gmail.com",
+        },
+        {
+          left: "Github",
+          right: "https://github.com/Near99",
+          address: "https://github.com/Near99",
+        },
+        {
+          left: "Linkedin",
+          right: "https://www.linkedin.com/in/kaixing-wong-b8b983145/",
+          address: "https://www.linkedin.com/in/kaixing-wong-b8b983145/",
+        },
+      ],
+    },
+  },
+
+  chinese: {
+    nav: {
+      about: "关于",
+      project: "项目",
+      resume: "简历",
+      contact: "联系",
+    },
+    home: {
+      nameHeading: `我系渣渣灰`,
+      briefIntroOne: `
+      骚枫盈橘葡输属波金殊港莎标伤叛辉里客滋锈踏藤浙脱视纱，滩第薄颌慕淬外，且梭商斑雌牙枪掠挑瓶滑短追允钻绝肆…界烙艺滥瓜缠燃药匪佐噪叔兔吏杠肝助秆姐瞎崎沾涅馈衍砷枕网铅兆蜜俭璃乌栓！
+        `,
+      briefIntroTwo: ` 
+      醋支集央笛京勾末胸脆份茵说凯伪颊陨缸宝醉谷择耿躺邻稿？亭草捐坚丧归贤毒颠膏充抹颁集醇脂俊埃弦桥很漆膜缝官亲蒜聚履奏兽匀菇衷陵永村鼓加似耸金裂御荒，便弗祥糖堆厌档衣模焦柱饿顶写、授冈钮坛坑鞋北屯张唤铺奉盗述型谎。
+              `,
+      moreAboutMe: "了解更多",
+    },
+    about: {
+      aboutTitle: "关于我",
+      aboutInfo: [
+        {
+          left: "波金",
+          right: "查看波金",
+          p: `牛四告訴黃動美十夏同，吉尾還首春種雨祖走春、哥黃卜良位來牠枝水校斥造，聲申爬。木發土胡。面金師十英喝急英只，未彩明經！吃羊歌沒品請行前晚泉許誰生菜朵頁身，反用兆南綠旁？包掃央裏害青月歡。南怕何休歌跟喜到公自小公太冰第、首她大早魚禾昔光同何奶爬借前波：鴨收跑枝兒父後，就棵因知珠千？把汗跳爬方金耳跟吹占成休婆，兆去發肉這字平忍戊行節節，發對一才。斥新共國果洋貝邊化音小地紅？時瓜民國跟。`,
+        },
+        {
+          left: "波金",
+          right: "查看波金",
+          p: `牛四告訴黃動美十夏同，吉尾還首春種雨祖走春、哥黃卜良位來牠枝水校斥造，聲申爬。木發土胡。面金師十英喝急英只，未彩明經！吃羊歌沒品請行前晚泉許誰生菜朵頁身，反用兆南綠旁？包掃央裏害青月歡。南怕何休歌跟喜到公自小公太冰第、首她大早魚禾昔光同何奶爬借前波：鴨收跑枝兒父後，就棵因知珠千？把汗跳爬方金耳跟吹占成休婆，兆去發肉這字平忍戊行節節，發對一才。斥新共國果洋貝邊化音小地紅？時瓜民國跟。`,
+        },
+        {
+          left: "波金",
+          right: "查看波金",
+          p: `牛四告訴黃動美十夏同，吉尾還首春種雨祖走春、哥黃卜良位來牠枝水校斥造，聲申爬。木發土胡。面金師十英喝急英只，未彩明經！吃羊歌沒品請行前晚泉許誰生菜朵頁身，反用兆南綠旁？包掃央裏害青月歡。南怕何休歌跟喜到公自小公太冰第、首她大早魚禾昔光同何奶爬借前波：鴨收跑枝兒父後，就棵因知珠千？把汗跳爬方金耳跟吹占成休婆，兆去發肉這字平忍戊行節節，發對一才。斥新共國果洋貝邊化音小地紅？時瓜民國跟。`,
+        },
+        {
+          left: "波金",
+          right: "查看波金",
+          p: `牛四告訴黃動美十夏同，吉尾還首春種雨祖走春、哥黃卜良位來牠枝水校斥造，聲申爬。木發土胡。面金師十英喝急英只，未彩明經！吃羊歌沒品請行前晚泉許誰生菜朵頁身，反用兆南綠旁？包掃央裏害青月歡。南怕何休歌跟喜到公自小公太冰第、首她大早魚禾昔光同何奶爬借前波：鴨收跑枝兒父後，就棵因知珠千？把汗跳爬方金耳跟吹占成休婆，兆去發肉這字平忍戊行節節，發對一才。斥新共國果洋貝邊化音小地紅？時瓜民國跟。`,
+        },
+        {
+          left: "波金",
+          right: "查看波金",
+          p: `牛四告訴黃動美十夏同，吉尾還首春種雨祖走春、哥黃卜良位來牠枝水校斥造，聲申爬。木發土胡。面金師十英喝急英只，未彩明經！吃羊歌沒品請行前晚泉許誰生菜朵頁身，反用兆南綠旁？包掃央裏害青月歡。南怕何休歌跟喜到公自小公太冰第、首她大早魚禾昔光同何奶爬借前波：鴨收跑枝兒父後，就棵因知珠千？把汗跳爬方金耳跟吹占成休婆，兆去發肉這字平忍戊行節節，發對一才。斥新共國果洋貝邊化音小地紅？時瓜民國跟。`,
+        },
+        {
+          left: "波金",
+          right: "查看波金",
+          p: `牛四告訴黃動美十夏同，吉尾還首春種雨祖走春、哥黃卜良位來牠枝水校斥造，聲申爬。木發土胡。面金師十英喝急英只，未彩明經！吃羊歌沒品請行前晚泉許誰生菜朵頁身，反用兆南綠旁？包掃央裏害青月歡。南怕何休歌跟喜到公自小公太冰第、首她大早魚禾昔光同何奶爬借前波：鴨收跑枝兒父後，就棵因知珠千？把汗跳爬方金耳跟吹占成休婆，兆去發肉這字平忍戊行節節，發對一才。斥新共國果洋貝邊化音小地紅？時瓜民國跟。`,
+        },
+        {
+          left: "波金",
+          right: "查看波金",
+          p: `牛四告訴黃動美十夏同，吉尾還首春種雨祖走春、哥黃卜良位來牠枝水校斥造，聲申爬。木發土胡。面金師十英喝急英只，未彩明經！吃羊歌沒品請行前晚泉許誰生菜朵頁身，反用兆南綠旁？包掃央裏害青月歡。南怕何休歌跟喜到公自小公太冰第、首她大早魚禾昔光同何奶爬借前波：鴨收跑枝兒父後，就棵因知珠千？把汗跳爬方金耳跟吹占成休婆，兆去發肉這字平忍戊行節節，發對一才。斥新共國果洋貝邊化音小地紅？時瓜民國跟。`,
+        },
+      ],
+    },
+
+    project: {
+      projectTitle: "吉尾還首春種雨",
+      projectInfo: [
+        {
+          left: "波金",
+          right: "查看波金",
+          p: `牛四告訴黃動美十夏同，吉尾還首春種雨祖走春、哥黃卜良位來牠枝水校斥造，聲申爬。木發土胡。面金師十英喝急英只，未彩明經！吃羊歌沒品請行前晚泉許誰生菜朵頁身，反用兆南綠旁？包掃央裏害青月歡。南怕何休歌跟喜到公自小公太冰第、首她大早魚禾昔光同何奶爬借前波：鴨收跑枝兒父後，就棵因知珠千？把汗跳爬方金耳跟吹占成休婆，兆去發肉這字平忍戊行節節，發對一才。斥新共國果洋貝邊化音小地紅？時瓜民國跟。`,
+        },
+        {
+          left: "波金",
+          right: "查看波金",
+          p: `牛四告訴黃動美十夏同，吉尾還首春種雨祖走春、哥黃卜良位來牠枝水校斥造，聲申爬。木發土胡。面金師十英喝急英只，未彩明經！吃羊歌沒品請行前晚泉許誰生菜朵頁身，反用兆南綠旁？包掃央裏害青月歡。南怕何休歌跟喜到公自小公太冰第、首她大早魚禾昔光同何奶爬借前波：鴨收跑枝兒父後，就棵因知珠千？把汗跳爬方金耳跟吹占成休婆，兆去發肉這字平忍戊行節節，發對一才。斥新共國果洋貝邊化音小地紅？時瓜民國跟。`,
+        },
+        {
+          left: "波金",
+          right: "查看波金",
+          p: `牛四告訴黃動美十夏同，吉尾還首春種雨祖走春、哥黃卜良位來牠枝水校斥造，聲申爬。木發土胡。面金師十英喝急英只，未彩明經！吃羊歌沒品請行前晚泉許誰生菜朵頁身，反用兆南綠旁？包掃央裏害青月歡。南怕何休歌跟喜到公自小公太冰第、首她大早魚禾昔光同何奶爬借前波：鴨收跑枝兒父後，就棵因知珠千？把汗跳爬方金耳跟吹占成休婆，兆去發肉這字平忍戊行節節，發對一才。斥新共國果洋貝邊化音小地紅？時瓜民國跟。`,
+        },
+        {
+          left: "波金",
+          right: "查看波金",
+          p: `牛四告訴黃動美十夏同，吉尾還首春種雨祖走春、哥黃卜良位來牠枝水校斥造，聲申爬。木發土胡。面金師十英喝急英只，未彩明經！吃羊歌沒品請行前晚泉許誰生菜朵頁身，反用兆南綠旁？包掃央裏害青月歡。南怕何休歌跟喜到公自小公太冰第、首她大早魚禾昔光同何奶爬借前波：鴨收跑枝兒父後，就棵因知珠千？把汗跳爬方金耳跟吹占成休婆，兆去發肉這字平忍戊行節節，發對一才。斥新共國果洋貝邊化音小地紅？時瓜民國跟。`,
+        },
+        {
+          left: "波金",
+          right: "查看波金",
+          p: `牛四告訴黃動美十夏同，吉尾還首春種雨祖走春、哥黃卜良位來牠枝水校斥造，聲申爬。木發土胡。面金師十英喝急英只，未彩明經！吃羊歌沒品請行前晚泉許誰生菜朵頁身，反用兆南綠旁？包掃央裏害青月歡。南怕何休歌跟喜到公自小公太冰第、首她大早魚禾昔光同何奶爬借前波：鴨收跑枝兒父後，就棵因知珠千？把汗跳爬方金耳跟吹占成休婆，兆去發肉這字平忍戊行節節，發對一才。斥新共國果洋貝邊化音小地紅？時瓜民國跟。`,
+        },
+        {
+          left: "波金",
+          right: "查看波金",
+          p: `牛四告訴黃動美十夏同，吉尾還首春種雨祖走春、哥黃卜良位來牠枝水校斥造，聲申爬。木發土胡。面金師十英喝急英只，未彩明經！吃羊歌沒品請行前晚泉許誰生菜朵頁身，反用兆南綠旁？包掃央裏害青月歡。南怕何休歌跟喜到公自小公太冰第、首她大早魚禾昔光同何奶爬借前波：鴨收跑枝兒父後，就棵因知珠千？把汗跳爬方金耳跟吹占成休婆，兆去發肉這字平忍戊行節節，發對一才。斥新共國果洋貝邊化音小地紅？時瓜民國跟。`,
+        },
+        {
+          left: "波金",
+          right: "查看波金",
+          p: `牛四告訴黃動美十夏同，吉尾還首春種雨祖走春、哥黃卜良位來牠枝水校斥造，聲申爬。木發土胡。面金師十英喝急英只，未彩明經！吃羊歌沒品請行前晚泉許誰生菜朵頁身，反用兆南綠旁？包掃央裏害青月歡。南怕何休歌跟喜到公自小公太冰第、首她大早魚禾昔光同何奶爬借前波：鴨收跑枝兒父後，就棵因知珠千？把汗跳爬方金耳跟吹占成休婆，兆去發肉這字平忍戊行節節，發對一才。斥新共國果洋貝邊化音小地紅？時瓜民國跟。`,
+        },
+      ],
+    },
+    contact: {
+      title: "How to react me",
+      contactInfo: [
+        { left: "Phone", right: "+86-0120-2433", address: "+86-0120-2433" },
+        { left: "Wechat", right: "18601202433", address: "18601202433" },
+        {
+          left: "Email",
+          right: "wkx0823@gmail.com",
+          address: "wkx0823@gmail.com",
+        },
+        {
+          left: "Github",
+          right: "https://github.com/Near99",
+          address: "https://github.com/Near99",
+        },
+        {
+          left: "Linkedin",
+          right: "https://www.linkedin.com/in/kaixing-wong-b8b983145/",
+          address: "https://www.linkedin.com/in/kaixing-wong-b8b983145/",
+        },
+      ],
+    },
+  },
+};
